be2ce4e3e88ad5bfe05789f58981d527da71b65d
Khoa <nguyenvietkhoa12031993@gmail.com>
2021-02-11 05:24:54 +0700

    fix api

--- src/api.js
+++ src/api.js
@@ -5,12 +5,10 @@ const mapboxApiKey = process.env.VUE_APP_MAPBOX_API_KEY;
 
 const getWeatherData = async (lon, lat) => {
   const weather = await axios.get(
-    `https://api.openweathermap.org/data/2.5/onecall?
-      &lon=${lon}&lat=${lat}&exclude=hourly,minutely,alerts&units=metric&appid=${owmApiKey}`
+    `https://api.openweathermap.org/data/2.5/onecall?&lon=${lon}&lat=${lat}&exclude=hourly,minutely,alerts&units=metric&appid=${owmApiKey}`
   );
   const location = await axios.get(
-    `https://api.mapbox.com/geocoding/v5/mapbox.places/
-      ${lon},${lat}.json?access_token=${mapboxApiKey}&types=country,place&limit=1`
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${lon},${lat}.json?access_token=${mapboxApiKey}&types=country,place&limit=1`
   );
 
   return axios.all([weather, location]).then(
